d35806318bce089a5f7f61c6af9069d6f2762407
designerlinak <147345246+mnikrn@users.noreply.github.com>
2024-04-27 13:35:25 -0700

    plz

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3.3",
+  base: "/3.3/",
   server: {
     port:3000,
   },
